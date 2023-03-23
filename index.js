2c75e2cd93f8a5792dbb2882f3dc0b50cb472683
Ross Rogerson <ross.rogerson@hotmail.com>
2023-03-23 15:31:43 +0000

    server up and running

---
+++ index.js
@@ -0,0 +1,39 @@
+import express from 'express'
+import mongoose from 'mongoose'
+import router from './config/router.js'
+import 'dotenv/config'
+
+const app = express()
+
+app.use(express.json())
+
+const startServer = async () => {
+  try {
+
+    // ? Connect to our mongodb database
+    await mongoose.connect(process.env.MONGO_URI)
+    console.log('Database connected')
+
+    // ? Middleware
+    // logger
+    app.use((req, res, next) => {
+      console.log(`incoming requests: ${req.method} ${req.url}`)
+      next()
+    })
+
+    // Routes
+    app.use('/api', router)
+
+    // 404 catach all middleware
+    app.use((req, res) => {
+      return res.status(404).json({ message: 'Route not found' })
+    })
+
+    app.listen(process.env.PORT, () => console.log(`Server is up and running and listening to port ${process.env.PORT}`))
+  } catch (err) {
+    console.log('There was an error starting the database')
+    console.log(err)
+  }
+}
+
+startServer()
