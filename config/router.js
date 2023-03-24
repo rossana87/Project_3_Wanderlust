eae3e0dacc3dca5a0145cda3f8ee521fcd5fdf56
Ross Rogerson <ross.rogerson@hotmail.com>
2023-03-24 13:47:00 +0000

    login and register route

--- config/router.js
+++ config/router.js
@@ -6,9 +6,5 @@ const router = express.Router()
 router.route('/')
   .get(displayDestination)
   .post(loginOrRegister)
-  // .post(registerUser)
-
-// router.route('/register')
-//   .post(registerUser)
 
 export default router
